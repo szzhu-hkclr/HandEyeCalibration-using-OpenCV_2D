77135d20a9f6abe5c27252353521a7ac0b357c5d
jude <szzhu@hkclr.hk>
2024-06-17 11:56:54 +0800

    - solve pnp from zero distortion images

--- HandEyeCalibration_class.py
+++ HandEyeCalibration_class.py
@@ -218,7 +218,7 @@ class CameraCalibration:
         i = 1
         for corners in chessboard_corners:
             # Best-fit(?) solve PnP method for chessboard scenario :SOLVEPNP_IPPE
-            _, rvec, tvec = cv2.solvePnP(object_points, corners, intrinsic_matrix, dist_coeffs, None, None, False,  cv2.SOLVEPNP_IPPE)
+            _, rvec, tvec = cv2.solvePnP(object_points, corners, intrinsic_matrix, None, None, None, False, cv2.SOLVEPNP_IPPE)
             # rvec is the rotation vector, tvec is the translation vector
             if Testing == True:
                 print("Current iteration: ", i, " out of ", len(chessboard_corners[0]), " iterations.")
